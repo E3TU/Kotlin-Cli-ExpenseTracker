97229258c5060b61776dc4da9482c94444b4e6f5
Eetu <99178022+E3TU@users.noreply.github.com>
2025-11-30 22:04:34 +0200

    Added Functions
    Added functions for adding and viewing expenses

--- main.kt
+++ main.kt
@@ -2,11 +2,35 @@ data class Expense(val description: String, val amount: Double)
 
 class ExpenseTracker {
     private val expenses = mutableListOf<Expense>()
+
+    fun addExpense(description: String, amount: Double) {
+        expenses.add(Expense(description, amount))
+        println("Expense added")
+    }
+
+    fun viewExpenses() {
+        if (expenses.isEmpty()) {
+            println("No expenses.")
+        } else {
+            println("Expenses:")
+            expenses.forEachIndexed { index, expense ->
+                println("${index + 1}. ${expense.description} - ${expense.amount}€")
+            }
+        }
+    }
+
+    fun totalExpense() {
+        val total = expenses.sumOf { it.amount }
+        println("Total expense: $total€")
+    }
 }
 
-
 fun main() {
+
+    val expenseTracker = ExpenseTracker()
+
     while (true) {
+        println("")
         println("Welcome to expense tracker")
         println("1. Add an expense")
         println("2. View all expenses")
@@ -18,16 +42,21 @@ fun main() {
 
         when (option) {
             "1" -> {
-                
+                print("Enter expense description: ")
+                val description: String = readln()
+                print("Enter expense amount: ")
+                val amount: String = readln()
+                expenseTracker.addExpense(description, amount.toDouble())
             }
             "2" -> {
-                
+                expenseTracker.viewExpenses()
             }
             "3" -> {
-                
+                expenseTracker.totalExpense()
             }
             "4" -> {
-                
+                println("Exiting program...")
+                break
             }
             else -> {
                 println("")
@@ -35,4 +64,5 @@ fun main() {
                 println("")
             }
         }
+    }
 }
